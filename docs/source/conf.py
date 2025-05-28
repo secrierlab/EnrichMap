de453ad278aba1f005a43989e90f1ff20cd88149
cenk <61861741+cenk-celik@users.noreply.github.com>
2025-05-28 11:24:19 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,3 +34,38 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 
 epub_show_urls = 'footnote'
+
+# Generate the API documentation when building
+autosummary_generate = True
+autodoc_member_order = "bysource"
+# autodoc_default_flags = ['members']
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_use_rtype = True  # having a separate entry generally helps readability
+napoleon_use_param = True
+napoleon_custom_sections = [("Params", "Parameters")]
+todo_include_todos = False
+api_dir = HERE / "api"  # function_images
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_image",
+    "html_admonition",
+]
+myst_url_schemes = ("http", "https", "mailto", "ftp")
+myst_heading_anchors = 3
+nb_output_stderr = "remove"
+nb_execution_mode = "off"
+nb_merge_streams = True
+
+
+ogp_site_url = "https://enrichmap.readthedocs.io/en/stable/"
+ogp_image = "https://github.com/secrierlab/enrichmap/raw/main/img/enrichmap_logo.jpg"
+
+typehints_defaults = "braces"
+
+pygments_style = "default"
+pygments_dark_style = "native"
