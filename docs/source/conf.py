23eeae33590398738f635ca34a59e3a18d50bfa8
cenk <61861741+cenk-celik@users.noreply.github.com>
2025-05-28 13:27:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ pygments_dark_style = "native"
 
 # The theme is sphinx-book-theme, with patches for readthedocs-sphinx-search
 repository_url = "https://github.com/secrierlab/enrichmap"
-html_theme = "scanpydoc"
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "repository_url": repository_url,
     "use_repository_button": True,
@@ -96,7 +96,7 @@ html_theme_options = {
 html_static_path = ["_static"]
 html_show_sphinx = False
 html_logo = "https://github.com/secrierlab/enrichmap/raw/main/img/enrichmap_logo.svg"
-html_title = "scanpy"
+html_title = "EnrichMap"
 
 
 def setup(app: Sphinx):
